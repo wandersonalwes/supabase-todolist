53d62cbf72ff478c1cc73a6231d8a4eff7b1eabf
Wanderson Alves <wandersonalwes@hotmail.com>
2022-08-30 18:31:09 -0300

    feat: add header

--- src/pages/todo-list.tsx
+++ src/pages/todo-list.tsx
@@ -1,5 +1,6 @@
 import { FormEvent, useState } from "react";
 import { Button } from "../components/button";
+import { Header } from "../components/header";
 import { Input } from "../components/input";
 import { TodoItem, TodoType } from "../components/todo-item";
 
@@ -12,12 +13,12 @@ export const TodoList = () => {
 
     if (!todo) return;
     setTodos((state) => [
-      ...state,
       {
         id: new Date().getTime().toString(),
         is_complete: false,
         task: todo,
       },
+      ...state,
     ]);
     setTodo("");
   };
@@ -52,28 +53,31 @@ export const TodoList = () => {
   };
 
   return (
-    <form onSubmit={handleAddTodo} className="bg-slate-100 ">
-      <div className="min-h-screen flex items-center p-8 flex-col gap-3 max-w-2xl mx-auto">
-        <div className="flex gap-4 items-center w-full mb-8">
-          <Input
-            value={todo}
-            onChange={(event) => setTodo(event.target.value)}
-          />
-          <Button type="submit">Adicionar</Button>
+    <div className="bg-slate-100 min-h-screen">
+      <Header />
+      <form onSubmit={handleAddTodo}>
+        <div className="flex items-center p-8 flex-col gap-3 max-w-6xl mx-auto">
+          <div className="flex gap-4 items-center w-full mb-8">
+            <Input
+              value={todo}
+              onChange={(event) => setTodo(event.target.value)}
+            />
+            <Button type="submit">Adicionar</Button>
+          </div>
+          <div className="w-full rounded-lg space-y-2">
+            {todos.map((todo) => {
+              return (
+                <TodoItem
+                  todo={todo}
+                  onRemove={handleRemove}
+                  onComplete={handleComplete}
+                  onEdit={handleEdit}
+                />
+              );
+            })}
+          </div>
         </div>
-        <div className="w-full rounded-lg space-y-2">
-          {todos.map((todo) => {
-            return (
-              <TodoItem
-                todo={todo}
-                onRemove={handleRemove}
-                onComplete={handleComplete}
-                onEdit={handleEdit}
-              />
-            );
-          })}
-        </div>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 };
